7e64d638c3a7770b2c21c6d03909bb614b9aa47a
Jeremy Magland <jeremy.magland@gmail.com>
2017-09-13 06:16:00 -0400

    fix typo

--- full_tests/simple_sort_001/synthesize_raw.sh
+++ full_tests/simple_sort_001/synthesize_raw.sh
@@ -2,4 +2,4 @@
 
 PATH0="MAGLAND_SIM001b"
 
-mp-run-process mountainsort.synthesize_timeseries --firings=$PATH0/firings_true.mda --waveforms=$PATH0/waveforms_true.mda --waveforms_upsample_factor=13 --noise_level=1 --timeseries_out=data/raw.mda
+mp-run-process mountainsort.synthesize_timeseries --firings=$PATH0/firings_true.mda --waveforms=$PATH0/waveforms_true.mda --waveform_upsample_factor=13 --noise_level=1 --timeseries_out=data/raw.mda
